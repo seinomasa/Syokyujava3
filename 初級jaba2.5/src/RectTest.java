0d1daef08683ea0fdee057dcdcd44c98101d5cdc
osamu <kopoandms@gmail.com>
2024-05-14 12:11:01 +0900

    確認問題３.3

--- 初級jaba2.5/src/RectTest.java
+++ 初級jaba2.5/src/RectTest.java
@@ -1,7 +1,17 @@
 
 public class RectTest extends MyFrame {
-	public void rub() {
-		fillRect(30,50,200,100);
+	public void run() {
+		setColor(255,0,255);
+		fillRect(30, 40, 50, 100);
+		setColor(0,255,255);
+		fillRect(130, 40, 50, 100);
+		setColor(0,0,0);
+		fillRect(30, 200, 150, 10);
+		setColor(255,255,0);
+		fillRect(90, 160, 30, 30);
+		setColor(123,123,123);
+		fillRect(30,40,100,10);
+		
 	}
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
--- 初級jaba2.5/src/start.java
+++ 初級jaba2.5/src/start.java
@@ -3,9 +3,9 @@ public class start {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-System.out.println("プログラムを実行します");
-MyFrame frame1=new RectTest();
-	
+		System.out.println("プログラムを実行します");
+		MyFrame frame1=new RectTest();
+
 	
   }
 }
