d4d714886aff6f75e4ed671f3c8e237a0625c005
osamu <kopoandms@gmail.com>
2024-05-14 10:44:51 +0900

    java3.1

---
+++ 初級jaba2.5/src/RectTest.java
@@ -0,0 +1,11 @@
+
+public class RectTest extends MyFrame {
+	public void rub() {
+		fillRect(30,50,200,100);
+	}
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
--- 初級jaba2.5/src/start.java
+++ 初級jaba2.5/src/start.java
@@ -3,17 +3,8 @@ public class start {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
-
-	int x =2;
-	int y =3;
-	System.out.println(x*y);
-	
-	MyFrame frame1=new RectFrame();
-	 frame1.setLocation(200,100);
-	 frame1.setSize(150,150);
-	 MyFrame frame2=new RectFrame();
-	 frame2.setLocation(350,200);
+System.out.println("プログラムを実行します");
+MyFrame frame1=new RectTest();
 	
 	
   }
