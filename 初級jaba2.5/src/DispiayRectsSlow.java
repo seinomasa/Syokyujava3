acad05b2afa47f0361bef281bb1090bf54313b22
osamu <kopoandms@gmail.com>
2024-05-21 12:09:45 +0900

    5.3

--- 初級jaba2.5/src/DispiayRectsSlow.java
+++ 初級jaba2.5/src/DispiayRectsSlow.java
@@ -1,9 +1,11 @@
 
 public class DispiayRectsSlow extends MyFrame {
 	public void run() {
-		setColor(0,128,0);
+		
 		int x=30;
 		while (x<170) {
+			clear();
+			setColor(0,128,0);
 			fillRect(x, 80, 10, 100);
 			x+=20;
 			sleep(1);
