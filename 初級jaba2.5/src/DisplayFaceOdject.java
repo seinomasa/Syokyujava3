7cb670f1ac841b0c755608e1783c18c988484dc5
osamu <kopoandms@gmail.com>
2024-05-28 10:46:01 +0900

    6,3

--- 初級jaba2.5/src/DisplayFaceOdject.java
+++ 初級jaba2.5/src/DisplayFaceOdject.java
@@ -1,16 +1,20 @@
 
 public class DisplayFaceOdject extends MyFrame{
 	public void run() {
-		Face face1=new Face();
-		face1.x=100;
-		face1.y=50;
-        face1.draw(this);
-        
-        
-        Face face2=new Face();
-        face2.x=200;
-		face2.y=1500;
-        face2.draw(this);
+       
+		Face face1=new Face(50,50);
+        Face face2 = new Face(200, 100);
+        for (int i = 0; i < 30; i++) {
+            clear();
+            face1.draw(this);
+            face2.draw(this);
+            face1.x += 10;
+            face1.y += 5;
+            face2.x -= 10;
+            face2.y -= 5;
+            sleep(0.1);
+        }
+       
 	}
 	}
 
