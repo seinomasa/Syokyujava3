d7e581ff07dd6ba866da354d4177e5ca6a1e85d1
osamu <kopoandms@gmail.com>
2024-06-10 10:44:02 +0900

    8.3

--- 初級jaba2.5/src/Car.java
+++ 初級jaba2.5/src/Car.java
@@ -1,27 +1,21 @@
-public class  Car {
-	int x,y,vx,vy;
-	 public Car (int x,int y,int vx,int vy) {
-		 this.x=x;
-		 this.y=y;
-		 this.vx=vx;
-		 this.vx=vy;
-	 }
-	 
+public class  Car extends Vehicle {
+	public  Car (int x,int y,int vx,int vy) {
+		super(x,y,vx,vy);
+	}
+
 	 public void draw(MyFrame frame) {
 		 
-		 frame.fillRect(x,y,40,30);        
-	        frame.fillRect(x-30,y+30,100,30);
-	        frame.fillOval(x-15,y+60,30,30);
-	        frame.fillOval(x+30,y+60,30,30);
+		 frame.fillRect(x+20,y,40,20);        
+	        frame.fillRect(x,y+20,80,20);
+	        frame.fillOval(x+10,y+40,20,20);
+	        frame.fillOval(x+50,y+40,20,20);
 	 }
-		public void move() {
-			x+=vx;
-			y+=vy;
+}	
 			
 			
-		}
 		
-	}
+		
+	
 	
 
 	
---
+++ 初級jaba2.5/src/Vehicle.java
@@ -0,0 +1,11 @@
+
+public class Vehicle {
+int x,y,vx,vy;
+public  Vehicle (int x,int y,int vx,int vy) {
+	
+}
+public void move() {
+	x+=vx;
+	y+=vy;
+}
+}
