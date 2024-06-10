bcc4dcb90addcfd3baed5c3828782ce92964389a
osamu <kopoandms@gmail.com>
2024-06-10 12:04:59 +0900

    リスト9.2

---
+++ 初級jaba2.5/src/PlayAnimation.java
@@ -0,0 +1,17 @@
+
+public class PlayAnimation extends MyFrame{
+public void run() {
+	Car player=new Car(100,200,0,0);
+	while(true) {
+		clear();
+		player.draw(this);
+		player.move();
+		sleep(0.1);
+	}
+			
+			
+			
+			
+			
+}
+}
